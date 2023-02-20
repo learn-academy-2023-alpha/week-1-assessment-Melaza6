99f326f12cba3650760f47f512cd4dec6b596679
Melaza6 <cueroleo6@gmail.com>
2023-02-20 15:42:10 -0800

    assesment completed

--- coding-concepts.js
+++ coding-concepts.js
@@ -17,16 +17,16 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 const cohort = "LEARN 2023"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: will give me the length of the string.  output // 10
+// b) Verify and explain: 10. .length is a built-in method that provides us the length of a string.
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
 // console.log(greeting[4])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: output "o"
+// b) Verify and explain: "o" the code given brings the value of the element sitting at the 4th index or the value of the element in the 5th location.
 
 // --------------------3) What will this log?
 
@@ -34,21 +34,21 @@ const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "Ruby"
+// b) Verify and explain: "Ruby" since the code is invoking the 1st index of the array and indexes start at 0
 
 // --------------------4) What will this log?
 
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "SATURDAY" "SUNDAY"
+// b) Verify and explain: weekendDays.toUpperCase is not a function, 
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Error
+// b) Verify and explain: "number"   I have no idea why! 
