46659ee20a1ca008edd394d1a101f716af9edb29
Melaza6 <cueroleo6@gmail.com>
2023-02-24 08:47:17 -0800

    resubmiting week 1 assesment

--- code-challenges.js
+++ code-challenges.js
@@ -21,6 +21,13 @@ const fruit4 = "kiwi"
 // --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
 // Pseudo code:
+// input: a number
+// output: where our number input is at boiling point or not
+// create a function boilingPoint
+// write a condetional statement that would take in tempeture
+// return if the tempeture is below, above or at boiling point
+
+
 // const boilingPoint = (num) => {
 //     if (num === 212){
 //         return `${num} is at boiling point`
@@ -49,7 +56,12 @@ const fruit4 = "kiwi"
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
-// Pseudo code:
+// Pseudo code: 
+// input: a combination of arrays
+// output: the length of a new array
+// concatanate the arrays given using built-in method .concat()
+// use built-in method .length
+// return the lenght of the new array.
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
@@ -63,17 +75,27 @@ const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
 // Pseudo code:
+// input: a given string
+// output: given string with letters in reverse "3202 ahplA"
+// split the given string with .split("") for each index in the string
+// reverse the given string with .reverse()
+// join the given string with .join("") to concatanate the given string
 
 const currentCohort = "Alpha 2023"
 
-console.log(currentCohort.reverse()) 
-   "working on"
+console.log(currentCohort.split("").reverse().join("")) 
+   
 
 // Expected output: "3202 ahplA"
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
 // Pseudo code:
+// input: a given array 
+// output: the last index of a given value
+// find the last index of a given value by using built-in method .lastIndexOf() 
+// input the given value inside the built-in method
+// return the last index of the given value.
 
 const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 const givenValue1 = 42
@@ -89,6 +111,11 @@ const givenValue2 = 10
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 // Pseudo code:
+// input: a given array of numbers
+// output: sort the numbers from largest to smallest
+// use built-in method .sort() to sort out the numbers
+// within the built-in method .sort() write a function(high order function) to sort out the number from largest to smallest
+// return the new array of numbers. 
 
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
 
